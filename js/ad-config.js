a4c934ab9449844dd836dd85b3427e26ce6624bf
JamesHung0521 <jspjames@126.com>
2026-08-06 12:04:25 +0800

    Add ad-config.js central ad switch

---
+++ js/ad-config.js
@@ -0,0 +1,6 @@
+// Ad Configuration - Central Switch
+// Change these values to control ads across the entire site
+window.AD_CONFIG = {
+    monetag: true,   // Monetag ads (Push, Vignette, In-Page Push)
+    adsense: false   // Google AdSense (enable after approval)
+};
